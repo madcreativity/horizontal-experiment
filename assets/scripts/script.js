6c2831d904a88e8cf642d6751a07b71f0e0cb850
4480135 <4480135@RTS-365.dk>
2019-08-23 12:48:14 +0200

    Initial commit.

---
+++ assets/scripts/script.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const DOMscrollAreaMain = document.querySelector("#scrollArea");
+    const DOMscrollAreaHorizontalBar = document.querySelector("#scrollArea .simplebar-scrollbar");
+    const scrollAreaMain_scrollElement = DOMscrollAreaMain.SimpleBar.getScrollElement();
+
+    // Ensures that the correct navbar link is highlighted
+    const DOMlinks = document.querySelectorAll("a[href^='#']");
+    const DOMnavLinks = document.querySelectorAll("nav a[href^='#']");
+
+    let calculateLinks = () => {
+        DOMnavLinks.forEach((link) => {
+            let el = document.querySelector(link.getAttribute("href"));
+            let elRect = el.getBoundingClientRect();
+            let mainAreaRect = scrollAreaMain_scrollElement.getBoundingClientRect();
+            
+            if(elRect.left <= mainAreaRect.right) {
+                link.classList.add("scrolled");
+            } else if(link.classList.contains("scrolled")) {
+                link.classList.remove("scrolled");
+            }
+        });
+    }
+
+    // Convert standard vertical scroll to horizontal scroll
+    DOMscrollAreaMain.addEventListener("onwheel" in document ? "wheel" : "mousewheel", function(e) {
+        e.preventDefault();
+        e.wheel = e.deltaY ? -e.deltaY : e.wheelDelta / 40;
+
+        if(scrollAreaMain_scrollElement.classList.contains("smooth")) {
+            scrollAreaMain_scrollElement.classList.remove("smooth");
+        }
+
+        scrollAreaMain_scrollElement.scrollLeft -= e.wheel;
+    });
+
+    // Apply event listener to links to make them scroll
+    DOMlinks.forEach((link) => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            let el = document.querySelector(this.getAttribute("href"));
+
+            if(!scrollAreaMain_scrollElement.classList.contains("smooth")) {
+                scrollAreaMain_scrollElement.classList.add("smooth");
+            }
+
+            scrollAreaMain_scrollElement.scrollLeft = el.offsetLeft - 60;
+
+            calculateLinks();
+        });
+    });
+
+    // Runs when any scrolling happens
+    scrollAreaMain_scrollElement.addEventListener("scroll", () => {
+        calculateLinks();
+    });
+
+    // Fix glitch where dragging the scrollbar manually moves it in an unnatural way
+    DOMscrollAreaHorizontalBar.addEventListener("mouseover", () => {
+        if(scrollAreaMain_scrollElement.classList.contains("smooth")) {
+            scrollAreaMain_scrollElement.classList.remove("smooth");
+        }
+    });
+
+    // First execution to ensure that the correct nav link is highlighted
+    calculateLinks();
+});
